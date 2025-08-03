eb37c0d3f317a7114cae121ebc9a748b54f484dc
Ghassene Ouadia <ghassene.o@belsons.com>
2025-08-03 17:24:49 +0100

    refactoring

--- OgistoInjection/ServiceDI.cs
+++ OgistoInjection/ServiceDI.cs
@@ -10,51 +10,49 @@ public static class ServiceDI
     {
         assembly ??= Assembly.GetCallingAssembly();
 
-        var typesWithAttribute = assembly.GetTypes()
+        var classWithAttribute = assembly.GetTypes()
             .Where(t => t.IsClass && !t.IsAbstract && t.GetCustomAttribute<InjectableAttribute>() != null);
 
-        foreach (var type in typesWithAttribute)
+        foreach (var classType in classWithAttribute)
         {
-            var attr = type.GetCustomAttribute<InjectableAttribute>();
-            var interfaceTypes = type.GetInterfaces();
+            var attr = classType.GetCustomAttribute<InjectableAttribute>();
+            var interfaceTypes = classType.GetInterfaces();
 
             if (!interfaceTypes.Any())
             {
-                Register(services, attr!.Type, type, null);
+                Register(services, attr!.Type, classType, null);
                 continue;
             }
 
             foreach (var interfaceType in interfaceTypes)
             {
-                Register(services, attr!.Type, type, interfaceType);
+                Register(services, attr!.Type, classType, interfaceType);
             }
         }
     }
 
-    private static void Register(IServiceCollection services, DITypes type, Type impl, Type? abstraction)
+    private static void Register(IServiceCollection services, DITypes type, Type classType, Type? interfaceType)
     {
         switch (type)
         {
             case DITypes.Scoped:
-                if (abstraction != null)
-                    services.AddScoped(abstraction, impl);
+                if (interfaceType != null)
+                    services.AddScoped(interfaceType, classType);
                 else
-                    services.AddScoped(impl);
+                    services.AddScoped(classType);
                 break;
             case DITypes.Singleton:
-                if (abstraction != null)
-                    services.AddSingleton(abstraction, impl);
+                if (interfaceType != null)
+                    services.AddSingleton(interfaceType, classType);
                 else
-                    services.AddSingleton(impl);
+                    services.AddSingleton(classType);
                 break;
             case DITypes.Transient:
-                if (abstraction != null)
-                    services.AddTransient(abstraction, impl);
+                if (interfaceType != null)
+                    services.AddTransient(interfaceType, classType);
                 else
-                    services.AddTransient(impl);
+                    services.AddTransient(classType);
                 break;
         }
-
-        Console.WriteLine($"Registered {(abstraction?.Name ?? impl.Name)} as {type} -> {impl.Name}");
     }
 }
